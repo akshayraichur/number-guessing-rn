e9cdba0eac327a9fc8178ce83c578c20c0dd5961
Akshay Raichur <akshay.raichur9@gmail.com>
2024-07-27 19:14:41 +0530

    chore: fix styling

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,59 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 import React from "react";
 import PrimaryButton from "@/components/PrimaryButton";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
 
 const HomePage = () => {
 	return (
-		<SafeAreaView>
-			<View>
-				<Text>HomePage</Text>
-				<PrimaryButton text='Hi there' />
-			</View>
-		</SafeAreaView>
+		<>
+			{/* <StatusBar style='light' /> */}
+			<SafeAreaView>
+				<View style={styles.inputContainer}>
+					<TextInput
+						placeholder='Enter Number..'
+						style={styles.textInput}
+						maxLength={2}
+						keyboardType='number-pad'
+						placeholderTextColor='#808080'
+					/>
+					{/* <Text style={styles.title}>Start Game Screen</Text> */}
+					<PrimaryButton text='Reset' />
+					<PrimaryButton text='Confirm' />
+				</View>
+			</SafeAreaView>
+		</>
 	);
 };
 
 export default HomePage;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	inputContainer: {
+		padding: 16,
+		marginTop: 16,
+		marginHorizontal: 24,
+		backgroundColor: "#4e0329",
+		borderRadius: 8,
+		elevation: 4, // boxShadow -> only available in android, not in ios
+		// the below is for ios shadow
+		shadowOffset: { width: 0, height: 2 },
+		shadowColor: "black",
+		shadowOpacity: 0.26,
+		shadowRadius: 6,
+	},
+	textInput: {
+		height: 50,
+		fontSize: 24,
+		borderBottomColor: "#ddb52f",
+		borderBottomWidth: 2,
+		color: "#ddb52f",
+		marginVertical: 8,
+		fontWeight: "bold",
+		// width: 150,
+		textAlign: "center",
+	},
+	title: {
+		color: "white",
+	},
+});
--- components/PrimaryButton.tsx
+++ components/PrimaryButton.tsx
@@ -1,18 +1,46 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Pressable } from "react-native";
 import React from "react";
 
-interface PrimaryButtonProps { 
-  text: string;
+interface PrimaryButtonProps {
+	text: string;
 }
 
 const PrimaryButton = ({ text }: PrimaryButtonProps) => {
+	const onPressHandler = () => {
+		console.log("Button Pressed");
+	};
 	return (
-		<View>
-			<Text>{text}</Text>
+		<View style={styles.container}>
+			<Pressable
+				onPress={onPressHandler}
+				style={({ pressed }) => (pressed ? [styles.button, styles.pressed] : styles.button)}
+				android_ripple={{ color: "#dda32f" }}>
+				<Text style={styles.text}>{text}</Text>
+			</Pressable>
 		</View>
 	);
 };
 
 export default PrimaryButton;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	container: {
+		borderRadius: 28,
+		marginVertical: 8,
+		overflow: "hidden",
+	},
+	button: {
+		backgroundColor: "#ddb52f",
+		paddingVertical: 10,
+		paddingHorizontal: 20,
+		elevation: 4,
+	},
+	text: {
+		color: "black",
+		textAlign: "center",
+		fontWeight: "bold",
+	},
+	pressed: {
+		opacity: 0.75,
+	},
+});
